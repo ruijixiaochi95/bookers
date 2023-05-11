e656c0bbcf33c4d20fe9e01f767f75245dbff203
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-05-11 09:37:06 +0000

    [add]2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,13 +11,28 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-
+    @books = Book.all
+    
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
