8672e63da36d95253889934793572504559489a4
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-05-12 11:59:12 +0000

    [FINISH]1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,14 +5,19 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to '/books'
+    if @book.save
+     flash[:notice] = "book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     render :index
+    end
   end
 
   def index
     @book = Book.new
     @books = Book.all
-    
+
   end
 
   def show
@@ -22,17 +27,27 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+     flash[:notice] = "book was successfully updated"
     redirect_to book_path(book.id)
+    else
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    if book.destroy
+     flash[:notice] = "book was successfully destroyed."
+     redirect_to '/books'
+    else
+     render :index
+    end
   end
 
   private
